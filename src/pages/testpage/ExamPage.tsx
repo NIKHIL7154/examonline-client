84773be38dde2daa7dee9418ce4afcae072854a8
NIKHIL THAKUR <nikhilthakurq@gmail.com>
2024-11-15 18:09:52 +0530

    Project initialized

---
+++ src/pages/testpage/ExamPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+    //define prop types here
+}
+
+const ExamPage = (props: Props) => {
+  return (
+    <div>ExamPage</div>
+  )
+}
+
+export default ExamPage
