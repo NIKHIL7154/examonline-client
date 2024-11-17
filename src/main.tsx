94547e0a5b86f97eb51b36e702aec9752b8d7d76
NIKHIL THAKUR <nikhilthakurq@gmail.com>
2024-11-17 18:46:39 +0530

    Authentication added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+import AuthLogin from './pages/authpage/AuthLogin.tsx'
+
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk publishable key to the .env.local file')
+}
 
 createRoot(document.getElementById('root')!).render(
   
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      {/* <App /> */}
+      <AuthLogin/>
+    </ClerkProvider>
   
 )
