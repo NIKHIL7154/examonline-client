20663102008c72abdb2a8d1c0ab3ae6b47610366
NIKHIL THAKUR <nikhilthakurq@gmail.com>
2024-12-03 16:44:23 +0530

    Feature -> TestsView Page Implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,49 @@
-import { useEffect, useState } from "react";
-import useTabStatus from "./hooks/useTabStatus";
-import ExamPage from "./pages/testpage/ExamPage"
-import { useDisableContextMenu } from "./hooks/useDisableContextMenu";
-import { useSelectionDisable } from "./hooks/useSelectionDisable";
-let count : number = 2
-function App() {
-  const [examStatus, setexamStatus] = useState("continue");
-  //const tabStatus = useTabStatus();
-  //useDisableContextMenu(); //disable right click on exam page
-  //useSelectionDisable(); //disable text selection on exam page
-  /* useEffect(() => {
-    if (tabStatus === "false") {
-      count--;
-      if (count <= 0) {
-        setexamStatus("cancelled");
-      } else{
-        setexamStatus("paused");
-      }
-      
-    }
-  }, [tabStatus]) */
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RootLayout from './layouts/RootLayout'
+import DashboardLayout from './layouts/DashboardLayout'
+import IndexPage from './pages/indexpage/IndexPage'
+import AuthLayout from './layouts/AuthLayout'
+import AuthPage from './pages/authpage/AuthPage'
+import LoginPage from './pages/authpage/LoginPage'
+
+import ErrorBoundary from './pages/ErrorBoundary'
+
+const router = createBrowserRouter([
+    {
+      element: <RootLayout />,
+      children: [
+        { path: '/', element: <IndexPage /> },
+        {
+          path: "auth", element: <AuthLayout />, children: [
+            {path:"",element:<AuthPage/>},
+          ]
+        }
+        , {
+          element: <DashboardLayout />,
+          path: 'app',
+          children: [
+            { path: '', element: <LoginPage /> },
+          ],
+        },
+      ],
+      errorElement: <ErrorBoundary />
+    },
+  ], {
+    future: {
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+    },
+  })
+
+
+const App = () => {
   return (
-    <>
-      {/* <h2 className=''>A new thing is going to happen</h2> */}
-      <div className="w-full flex relative">
-        
-        {(() => {
-          switch (examStatus) {
-            case "continue":
-              return <ExamPage />;
-            case "paused":
-              return (
-                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                  <h1 className="text-2xl">Your exam has been paused. This is last warning otherwise your exam will be cancelled</h1>
-                  <button className="bg-gray-300 py-1 px-7 hover:bg-gray-400" onClick={() => setexamStatus("continue")}>Resume</button>
-                </div>
-              );
-            case "cancelled":
-              return (
-                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                  <h1 className="text-2xl">Your exam has been cancelled.</h1>
-                </div>
-              );
-            default:
-              return null;
-          }
-        })()}
-          
-      </div>
-    </>
+    <RouterProvider router={router} future={{
+        v7_startTransition: true,
+      }} />
   )
 }
 
