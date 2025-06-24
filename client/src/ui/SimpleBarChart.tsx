e7437b72400a65b016372b6123dbf6a63186b396
Nikhil Thakur <nikhilthakurq@gmail.com>
2025-06-24 22:42:32 +0530

    Fixed error

--- client/src/ui/SimpleBarChart.tsx
+++ client/src/ui/SimpleBarChart.tsx
@@ -5,18 +5,18 @@ import { testAnalyticsType } from '../features/analytics/AnalyticsBox';
 import { format, isSameDay } from 'date-fns';
 
 // Sample data for the Bar Chart
-const data = [
-    { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
-    { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
-    { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
-    { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
-    { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
-    { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
-    { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
-    { name: 'Page H', uv: 3490, pv: 4300, amt: 2100 },
-    // { name: 'Page I', uv: 2490, pv: 4300, amt: 2100 },
-    // { name: 'Page J', uv: 5490, pv: 4300, amt: 2100 },
-];
+// const data = [
+//     { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
+//     { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
+//     { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
+//     { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
+//     { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
+//     { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
+//     { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
+//     { name: 'Page H', uv: 3490, pv: 4300, amt: 2100 },
+//     // { name: 'Page I', uv: 2490, pv: 4300, amt: 2100 },
+//     // { name: 'Page J', uv: 5490, pv: 4300, amt: 2100 },
+// ];
 interface Props {
     testAnalytics: testAnalyticsType[];
 }
